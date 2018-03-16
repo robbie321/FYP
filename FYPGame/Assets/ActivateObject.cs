78a626ec7ee677e1a7d364696b6e404ecbdaf28e
Robbie Malone <robmalone7@gmail.com>
2018-03-16 12:49:22 +0000

    Minor update to wave spawner

---
+++ FYPGame/Assets/ActivateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour {
+    Transform t;
+   // GameObject go;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            t = gameObject.transform.GetChild(0);
+            t.gameObject.SetActive(true);
+        }
+       
+    }
+}
